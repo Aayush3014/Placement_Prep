d3f5922817987e00d6a5457fd805bd6b62958c1e
Aayush3014 <0201csai036@niet.co.in>
2022-08-22 21:54:47 +0530

    Commit

--- Inputs.java
+++ Inputs.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-public class Inputs{
+public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please Enter some input: ");
-        int rollno = input.nextInt();
-        System.out.println("Your rollno is "+ rollno);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum = " + sum);
         input.close();
     }
 }
