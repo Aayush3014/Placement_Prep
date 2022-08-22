69602597ed3cb86b49f903c972c3f381be8dae33
Aayush3014 <0201csai036@niet.co.in>
2022-08-22 20:01:04 +0530

    Initial Commit

---
+++ First_Prog.java
@@ -0,0 +1,12 @@
+// Packages are folder in which java files lies.
+
+
+import java.util.Scanner;
+
+public class First_Prog{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.next());
+        System.out.println(input.nextInt());
+    }
+}
