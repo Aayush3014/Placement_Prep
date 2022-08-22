3f9922be746d3879109f1b17331676cc062fbcb4
Aayush3014 <0201csai036@niet.co.in>
2022-08-22 20:16:33 +0530

    Commit

--- Inputs.java
+++ Inputs.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Inputs{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter some input: ");
         int rollno = input.nextInt();
         System.out.println("Your rollno is "+ rollno);
         input.close();
