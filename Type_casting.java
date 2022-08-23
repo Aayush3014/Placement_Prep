1da7983b66c0cebf77f47ea8088661231dd8df20
Aayush3014 <0201csai036@niet.co.in>
2022-08-23 19:55:19 +0530

    COmmit

---
+++ Type_casting.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Type_casting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // float num = input.nextFloat();
+
+        // int num = input.nextInt(); // It will Give error if we pass float value in input.
+
+        // type Casting
+
+        float num =  67;
+        System.out.println(num);
+        input.close();
+    }
+}
