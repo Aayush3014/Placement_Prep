6bef135a1bfc7d44de399bcdc24cf82222ee1360
Aayush3014 <0201csai036@niet.co.in>
2022-08-23 22:14:02 +0530

    Commit

---
+++ Ass_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// Take name as input and print a greeting message for that particular name.
+
+
+
+public class Ass_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        System.out.println("Hello "+s+" broo");
+        input.close();
+    }
+}
