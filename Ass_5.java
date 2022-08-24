2c91ffe93981c37993614aff8b380eff78c28111
Aayush3014 <0201csai036@niet.co.in>
2022-08-24 19:44:56 +0530

    Commit

--- Ass_5.java
+++ Ass_5.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Ass_5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        if (num1>num2){
+            System.out.println("Largest number is "+num1);
+
+        }
+
+       else{
+     
+        System.out.println("Largest number is "+num2);
+        input.close();
+    }
     }
 }
