e1ed2cdcbba4228ff708d4107664822a65b2c77e
Aayush3014 <0201csai036@niet.co.in>
2022-08-25 12:01:31 +0530

    Commit

--- Ass_7.java
+++ Ass_7.java
@@ -1,21 +1,26 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// // To calculate Fibonacci Series up to n numbers.
+// To calculate Fibonacci Series up to n numbers.
 
-// public class Ass_7 {
-//     public static void main(String[] args) {
-//         int a = 0;
-//         int b = 1;
-//         int c = a + b;
-//         int a = b;
-//         int b = c;
-//         int count_loop = 1;
-//         Scanner input = new Scanner(System.in);
-//         int count = input.nextInt();
-//         while (count_loop != count) {
-//             System.out.println(c);
-//             count_loop++;
-//         }
-//         input.close();
-//     }
-// }
+public class Ass_7 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c;
+
+        int count_loop = 1;
+        Scanner input = new Scanner(System.in);
+        int count = input.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        while (count_loop != count-1) {
+            c = a + b;
+            
+            System.out.println(c);
+            a = b;
+            b = c;
+            count_loop++;
+        }
+        input.close();
+    }
+}
