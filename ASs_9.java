f754cd9fb681e5d7b9cb19a562f12f7467e98eff
Aayush3014 <0201csai036@niet.co.in>
2022-09-05 22:58:55 +0530

    Commit

---
+++ ASs_9.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// To find Armstrong Number between two given number.
+
+
+public class ASs_9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
---
+++ Conditions.java
@@ -0,0 +1,13 @@
+public class Conditions {
+    public static void main(String[] args) {
+        // Syntax of if Statements
+        int salary = 5400;
+        if (salary>10000){
+            salary = salary+4000;
+        }
+        else{
+            salary = salary + 2000;
+        }
+        System.out.println(salary);
+    }
+}
