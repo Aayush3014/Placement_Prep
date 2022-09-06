f3e517bc592557f0846a9d8e0ca9f65276369419
Aayush3014 <0201csai036@niet.co.in>
2022-09-06 22:51:38 +0530

    Commit

--- ASs_9.java
+++ ASs_9.java
@@ -6,6 +6,6 @@ import java.util.Scanner;
 public class ASs_9 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        input.close();
     }
 }
---
+++ Loops.java
@@ -0,0 +1,10 @@
+// import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        // Scanner input
+        for (int num = 0 ,k=5;num<=  5 & k<10;num+=1,k+=1){
+            System.out.println(num+","+k);
+        }
+    }
+}
