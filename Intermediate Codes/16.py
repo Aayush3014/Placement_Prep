17cf3e6e5bebf82a5e02c542639109dff652d64e
Aayush3014 <0201csai036@niet.co.in>
2022-10-04 09:18:31 +0530

    Commit

---
+++ Intermediate Codes/16.py
@@ -0,0 +1,14 @@
+from platform import python_branch
+import string
+
+
+# Reverse A String In python
+# string = "ayush"
+# reversed_string = string[::-1]
+# print(reversed_string)
+
+normal_string = "ayush"
+reversed_string = ""
+for i in range(len(normal_string)-1,-1,-1):
+    reversed_string = reversed_string + normal_string[i]
+print(reversed_string)
