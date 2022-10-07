891ed039f66a4dd953a08c77a5fd1c989e4b37e8
Aayush3014 <0201csai036@niet.co.in>
2022-10-07 22:12:55 +0530

    Commit

--- Arrays in Python/Easy/array_initialisation.py
+++ Arrays in Python/Easy/array_initialisation.py
@@ -1,5 +1,18 @@
 from array import *
 
-vals = array('i',[5,8,9,7,4])
-vals.append(65)
-print(vals.buffer_info())
+vals = array('i',[5,8,9,7,4,0])   # Initializing a new array.
+
+newArr = array(vals.typecode,(a for a in vals))   # Creating a new array from old array.
+
+
+# Iterating using for loop 
+
+# for i in newArr:
+#     print(i,end=' ')
+
+
+# Iterating using for loop 
+i = 0
+while i<len(newArr):
+    print(newArr[i])
+    i+=1
