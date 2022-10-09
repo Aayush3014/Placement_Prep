5701c143c00f7c8bb9264ead48a3194ca33384b8
Aayush3014 <0201csai036@niet.co.in>
2022-10-09 15:50:36 +0530

    Commit

--- Arrays in Python/Easy/Build Array from Permutation.py
+++ Arrays in Python/Easy/Build Array from Permutation.py
@@ -1,8 +1,7 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        nums2 = []
-        for i in range(len(nums)):
-            a = nums[nums[i]]
-            if a not in nums2:
-                nums2.append(a)
-        return nums2
+nums = [0,2,1,5,3,4]
+nums2 = []
+for i in range(len(nums)):
+    a = nums[nums[i]]
+    if a not in nums2:
+        nums2.append(a)
+print(nums2)
---
+++ Arrays in Python/Easy/Concatenation of Array.py
@@ -0,0 +1,16 @@
+
+nums = [1, 2, 1]
+ans = []
+# # 1 
+# ans = nums+nums
+
+# # 2
+# ans = nums*2
+
+#3
+# for i in range(len(nums)):
+#     ans.append(nums[i])
+# for j in range(len(nums)):
+#     ans.append(nums[j])       
+       
+print(ans)
