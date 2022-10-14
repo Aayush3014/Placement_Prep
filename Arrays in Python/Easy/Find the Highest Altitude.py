4e8a8b6835a308c79eafc708b693db7a6b1d454d
Aayush3014 <ayushak3008@gmail.com>
2022-10-14 16:35:12 +0530

    Commit

--- Arrays in Python/Easy/Find the Highest Altitude.py
+++ Arrays in Python/Easy/Find the Highest Altitude.py
@@ -1,12 +1,15 @@
-class Solution:
-    def largestAltitude(self, gain: List[int]) -> int:
-        new=[]
-        sum = 0
-        new.append(sum)
-        for i in range(len(gain)):
-            sum+=gain[i]
-            new.append(sum)
-        return max(new)
+gain = [-4,-3,-2,-1,4,3,2]
+# new=[]
+greater = 0
+sum = 0
+# new.append(sum)
+for i in range(len(gain)):
+    sum+=gain[i]
+    if sum>greater:
+        greater = sum
+    # new.append(sum)
+print(greater)
 
 
 # Or we can also use a variable for storing the greater variable.
+
