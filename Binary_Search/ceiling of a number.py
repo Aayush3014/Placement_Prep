4b9cc95e0c5008b91d7167c11497b34b34e3a1fc
Aayush3014 <ayushak3008@gmail.com>
2023-02-24 21:30:03 +0530

    Commit

---
+++ Binary_Search/ceiling of a number.py
@@ -0,0 +1,16 @@
+def binary_search(arr,n):
+    beg = 0
+    end = len(arr)-1
+    while beg<=end:
+        mid = (beg)+(end-beg)//2
+        if arr[mid]==n:
+            return f"{arr[mid]} found at index {mid}"
+        elif arr[mid]<n:
+            beg = mid+1
+        elif arr[mid]>n:
+            end = mid-1
+    return beg
+
+a = [2,6,9,58,96]
+n = 90
+print(binary_search(a,n))
--- Binary_Search/ceiling_of_letter.py
+++ Binary_Search/ceiling_of_letter.py
@@ -1,40 +1,15 @@
-letter = ["c","f","j"]
-target = "a"
+letter = [3,6,10]
+target = 1
 beg = 0
 end = len(letter)-1
+ans = 0
 
-if target<letter[0] or target>letter[-1]:
-    # lst.append(letter[0])
-    return letter[0]
 while beg<=end:
     mid = (beg+end)//2
-    # if letter[mid]==target:
-    #     print(letter[mid])
-    # else:
-    if target>=letter[mid]:
+    if mid == target:
+        ans =  beg+1
+    elif mid > target:
         beg = mid + 1
-    if target<letter[mid]:
+    elif mid<target:
         end = mid-1
-        
-#     print(letter[end])
-# else:
-#     print(letter[0])
-return letter[beg]
-
-"""
-# if the number is out of bound
-        if target >= letters[-1] or target < letters[0]:
-            return letters[0]
-        
-        low = 0
-        high = len(letters)-1
-        while low <= high:
-            mid = (high+low)//2
-            
-            if  target >= letters[mid]: # in binary search this would be only greater than
-                low = mid+1
-            
-            if target < letters[mid]:
-                high = mid-1
-                
-        return letters[low]"""
+print(ans)
